95a1e8e8df8c30adb0f2421fb5fa5aea81cd0a9a
Whth <whthyotta@qq.com>
2026-01-10 21:46:40 +0800

    feat(rust): add Python stub generation support with pyo3-stub-gen
    - Updated pyo3 dependency from >=0.24.2 to 0.27.2
    - Added pyo3-stub-gen dependency for generating Python type stubs
    - Configured new 'stubgen' feature flag for stub generation
    - Added conditional compilation attributes for stub generation mode
    - Integrated stub info gatherer using define_stub_info_gatherer macro
    - Modified Python module registration to exclude when stubgen feature is enabled
    - Fixed Python version constraint syntax in pyproject.toml from <=3.14 to <3.14

--- {{ cookiecutter.full_name }}/src/lib.rs
+++ {{ cookiecutter.full_name }}/src/lib.rs
@@ -1,3 +1,5 @@
+#![cfg_attr(feature = "stubgen", allow(dead_code, unused,))]
+
 use pyo3::prelude::*;
 
 
@@ -5,11 +7,12 @@ mod {{ cookiecutter.project_name }};
 /// A Python module implemented in Rust. The name of this function must match
 /// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
 /// import the module.
+#[cfg(not(feature = "stubgen"))]
 #[pymodule]
-#[pyo3(name = "rust")]
 fn rust(python: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
     
     {{ cookiecutter.project_name }}::register(python, m)?;
     Ok(())
 
 }
+define_stub_info_gatherer!(stub_info);
